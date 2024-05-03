8f58a8d3c81fc0a900b1348e415244de2a0657b0
Cnidarias <roessner.pascal@gmail.com>
2024-05-03 14:32:16 +0200

    ✨ Exercise: 8.5 implement a transpose function for 3x3 matrix
    Clippy is unhappy with the naive solution that I also came up with and
    that is listed as the solution in the book because we are having to use
    extra bounds checks:
    
    https://rust-lang.github.io/rust-clippy/master/index.html#/needless_range_loop

---
+++ src/ex_8_5.rs
@@ -0,0 +1,47 @@
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut res = [[0; 3]; 3];
+
+    for i in 0..3 {
+        for j in 0..3 {
+            res[i][j] = matrix[j][i]
+        }
+    }
+
+    res
+}
+
+pub fn print_exercise() {
+    println!("Exercise");
+    println!("----- 8.5 -----");
+
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix: {:#?}", matrix);
+    let transposed = transpose(matrix);
+    println!("transposed: {:#?}", transposed);
+
+    println!("---------------");
+    println!();
+}
+
+#[test]
+fn test_transpose() {
+    let matrix = [
+        [101, 102, 103], //
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301], //
+            [102, 202, 302],
+            [103, 203, 303],
+        ]
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 pub mod ex_5_6;
 pub mod ex_6_7;
+pub mod ex_8_5;
 
 fn main() {
     ex_5_6::print_exercise();
     ex_6_7::print_exercise();
+    ex_8_5::print_exercise();
 }
