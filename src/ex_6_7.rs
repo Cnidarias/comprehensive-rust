4cb760660d23833134d05ffb0605c0c15a516b0f
Cnidarias <roessner.pascal@gmail.com>
2024-05-03 14:07:34 +0200

    ✨ Exercise: 6.7 Collatz Length with Tests

---
+++ src/ex_6_7.rs
@@ -0,0 +1,27 @@
+pub fn collatz_length(mut n: i32) -> u32 {
+    assert!(n > 0, "n must be greater than 0");
+
+    let mut len = 1;
+    while n > 1 {
+        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
+        len += 1;
+    }
+
+    len
+}
+
+#[test]
+fn test_collatz_length_with_3() {
+    assert_eq!(collatz_length(3), 8);
+}
+
+#[test]
+fn test_collatz_length_with_11() {
+    assert_eq!(collatz_length(11), 15);
+}
+
+#[test]
+#[should_panic(expected = "n must be greater than 0")]
+fn test_collatz_length_with_0() {
+    collatz_length(0);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 pub mod ex_5_6;
+pub mod ex_6_7;
+
 fn main() {
     let n = 20;
     println!("Exercise 5.6: fib({n}) = {}", ex_5_6::fib(n));
+
+    let n = 3;
+    println!(
+        "Exercise 6.7: collatz_length({n}) = {}",
+        ex_6_7::collatz_length(n)
+    );
 }
