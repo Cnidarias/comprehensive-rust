ba767425ba455d109f39dd053a1b5854bb9c8754
Cnidarias <roessner.pascal@gmail.com>
2024-05-03 14:36:19 +0200

    🚨 Transpose: Use iter with enumerator to make clippy happy

--- src/ex_8_5.rs
+++ src/ex_8_5.rs
@@ -1,9 +1,9 @@
 fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
     let mut res = [[0; 3]; 3];
 
-    for i in 0..3 {
-        for j in 0..3 {
-            res[i][j] = matrix[j][i]
+    for (i, row) in matrix.iter().enumerate() {
+        for (j, val) in row.iter().enumerate() {
+            res[j][i] = *val;
         }
     }
 
